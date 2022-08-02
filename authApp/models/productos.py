acff6c64479fc921a7c8c84018afb8c59f9a6549
Cristian Moreno <crismonsalve4>
2022-08-01 21:38:51 -0500

    prueba

--- authApp/models/productos.py
+++ authApp/models/productos.py
@@ -3,9 +3,10 @@ from .user import User
 from .categoria import Categoria
 
 class Productos(models.Model):
+    nombre=models.CharField(max_length = 50)
     id = models.AutoField(primary_key=True)
     descripcion = models.CharField('Descripción', max_length = 50)
-    codigo = models.IntegerField(default=0)
+    # codigo = models.IntegerField(default=0)
     categoria = models.ForeignKey(Categoria, related_name='product', on_delete=models.CASCADE)
     precio_venta = models.IntegerField(default=0)
     existencias = models.IntegerField(default=0)
